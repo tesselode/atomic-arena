06b8d6c1f702df686b0c6c2900160a0de676ae92
Andrew Minnich <aminnich3@gmail.com>
2021-05-23 17:25:46 -0400

    removal

--- src/lib.rs
+++ src/lib.rs
@@ -74,6 +74,12 @@ impl ControllerInner {
 		}
 		Err(ArenaFull)
 	}
+
+	fn free(&self, index: usize) {
+		let slot = &self.slots[index];
+		slot.free.store(true, Ordering::SeqCst);
+		slot.generation.fetch_add(1, Ordering::SeqCst);
+	}
 }
 
 #[derive(Debug, Clone)]
@@ -86,6 +92,10 @@ impl Controller {
 
 	pub fn try_reserve(&self) -> Result<Index, ArenaFull> {
 		self.0.try_reserve()
+	}
+
+	fn free(&self, index: usize) {
+		self.0.free(index);
 	}
 }
 
@@ -129,6 +139,23 @@ impl<T> Arena<T> {
 		}
 		slot.data = Some(data);
 		Ok(())
+	}
+
+	pub fn remove(&mut self, index: Index) -> Option<T> {
+		// TODO: answer the following questions:
+		// - if you reserve a key, then try to remove the key
+		// without having inserted anything, should the slot
+		// be unreserved? the current answer is no
+		// - what should happen if you try to remove a slot
+		// with the wrong generation? currently the answer is
+		// it just returns None like normal
+		let slot = &mut self.slots[index.index];
+		if slot.data.is_none() || slot.generation != index.generation {
+			return None;
+		}
+		slot.generation += 1;
+		self.controller.free(index.index);
+		slot.data.take()
 	}
 }
 
@@ -181,5 +208,42 @@ mod test {
 		assert_eq!(arena.slots[2].data, Some(3));
 		assert_eq!(arena.slots[2].generation, 0);
 		assert_eq!(arena.insert(index1, 4), Err(IndexNotReserved));
+	}
+
+	#[test]
+	fn remove() {
+		let mut arena = Arena::new(3);
+		let controller = arena.controller();
+		let index1 = controller.try_reserve().unwrap();
+		let index2 = controller.try_reserve().unwrap();
+		let index3 = controller.try_reserve().unwrap();
+		arena.insert(index1, 1).unwrap();
+		arena.insert(index2, 2).unwrap();
+		arena.insert(index3, 3).unwrap();
+		assert_eq!(arena.remove(index1), Some(1));
+		assert_eq!(arena.remove(index3), Some(3));
+		assert_eq!(arena.remove(index1), None);
+		assert_eq!(arena.slots[0].data, None);
+		assert_eq!(arena.slots[0].generation, 1);
+		assert_eq!(arena.slots[1].data, Some(2));
+		assert_eq!(arena.slots[1].generation, 0);
+		assert_eq!(arena.slots[2].data, None);
+		assert_eq!(arena.slots[2].generation, 1);
+		let index4 = controller.try_reserve();
+		assert_eq!(
+			index4,
+			Ok(Index {
+				index: 0,
+				generation: 1,
+			})
+		);
+		let index4 = index4.unwrap();
+		assert!(arena.insert(index4, 4).is_ok());
+		assert_eq!(arena.slots[0].data, Some(4));
+		assert_eq!(arena.slots[0].generation, 1);
+		assert_eq!(arena.slots[1].data, Some(2));
+		assert_eq!(arena.slots[1].generation, 0);
+		assert_eq!(arena.slots[2].data, None);
+		assert_eq!(arena.slots[2].generation, 1);
 	}
 }
